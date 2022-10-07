7e96415b45da56aa08e2f47fbe58f786011560ce
Jagane Sundar <jagane@infinstor.com>
2022-10-07 16:58:54 -0700

    create a low privilege user role for user code pods to run in

---
+++ docs/docs.mlflow-parallels.org/scripts/remove-user-serviceaccount.sh
@@ -0,0 +1,3 @@
+kubectl delete rolebinding k8s-serviceaccount-for-users-parallelsns-binding -n parallelsns
+kubectl delete serviceaccount k8s-serviceaccount-for-users-parallelsns -n parallelsns
+kubectl delete clusterrole k8s-role-for-users-parallelsns
