1db42a97a8559d76d3fa4a6a4d26e660a09ef24c
Jagane Sundar <jagane@infinstor.com>
2023-06-28 23:14:49 -0700

    more work on deploy model

--- processors/k8s/eks/deploymodel/stub.sh
+++ processors/k8s/eks/deploymodel/stub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /root
-curl -o /root/bootstrap.sh https://concurrentdist.s3.amazonaws.com/scripts/bootstrap.sh
+curl -o /root/bootstrap.sh https://concurrentdist.s3.amazonaws.com/scripts/deploymodel/bootstrap.sh
 chmod 755 /root/bootstrap.sh
 bash /root/bootstrap.sh
 exit $?
