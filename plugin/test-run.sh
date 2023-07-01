b5349b6912535d6bd851b91e13bd0a7fc67cd583
Jagane Sundar <jagane@infinstor.com>
2023-06-30 22:54:04 -0700

    more work on model deployment

--- plugin/test-run.sh
+++ plugin/test-run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mlflow deployments create --target concurrent-deployment -C kube-context=parallels-free -C kube-namespace=nsforconcurrent -C resources.requests.cpu=1500m -C resources.requests.memory=1024Mi -C resources.requests.nvidia.com/gpu=1 -C backend_type=gke --name test1 --model-uri models:/GPT2-Large/1
+#MODEL="models:/GPT2-Large/1"
+MODEL="models:/BartForCausalLM/1"
+mlflow deployments create --target concurrent-deployment -C kube-context=parallels-free -C kube-namespace=nsforconcurrent -C resources.requests.cpu=1500m -C resources.requests.memory=1024Mi -C resources.requests.nvidia.com/gpu=1 -C backend_type=gke --name test1 --model-uri "$MODEL"
