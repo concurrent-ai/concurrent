70d532ff14b327c7aaef522af106832408825532
Jagane Sundar <jagane@infinstor.com>
2023-07-01 23:50:12 -0700

    gpu drivers in inference container

--- processors/k8s/eks/deploymodel/stub.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /root
-curl -o /root/bootstrap.sh https://concurrentdist.s3.amazonaws.com/scripts/deploymodel/bootstrap.sh
-chmod 755 /root/bootstrap.sh
-bash /root/bootstrap.sh
-exit $?
