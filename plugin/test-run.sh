7e8df5de19c9573662933c7d2bb5384ac5d9447e
Jagane Sundar <jagane@infinstor.com>
2023-07-02 22:24:05 -0700

    more work on deploying transformers model

--- plugin/test-run.sh
+++ plugin/test-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #MODEL="models:/GPT2-Large/1"
 MODEL="models:/BartForCausalLM/1"
-mlflow deployments create --target concurrent-deployment -C kube-context=parallels-free -C kube-namespace=nsforconcurrent -C resources.requests.cpu=1500m -C resources.requests.memory=1024Mi -C resources.requests.nvidia.com/gpu=1 -C backend_type=gke --name test1 --model-uri "$MODEL"
+mlflow deployments create --target concurrent-deployment -C kube-context=parallels-free -C kube-namespace=nsforconcurrent -C resources.requests.cpu=3000m -C resources.requests.memory=6000Mi -C resources.requests.nvidia.com/gpu=1 -C backend_type=gke --name test1 --model-uri "$MODEL"
