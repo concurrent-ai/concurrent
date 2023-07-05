4edce4e9936af86c355e9822369be3f7903820d7
Jagane Sundar <jagane@infinstor.com>
2023-07-05 16:18:03 -0700

    list deployments. Not all cases work

--- plugin/concurrent_plugin/concurrent_deployment.py
+++ plugin/concurrent_plugin/concurrent_deployment.py
@@ -173,7 +173,28 @@ class PluginConcurrentDeploymentClient(BaseDeploymentClient):
         print(f"PluginConcurrentDeploymentClient.list_deployments: endpoint={endpoint}")
         if os.environ.get("raiseError") == "True":
             raise RuntimeError("Error requested")
-        return [{"name": f_deployment_name}]
+        cognito_client_id, _, _, _, region = get_conf()
+        token = get_token(cognito_client_id, region, True)
+        print(f"PluginConcurrentDeploymentClient.list_deployments: token={token}")
+
+        headers = {
+                'Content-Type': 'application/x-amz-json-1.1',
+                'Authorization' : 'Bearer ' + token
+                }
+        url = get_env_var().rstrip('/') + '/api/2.0/mlflow/parallels/list-deployments'
+        print(f"PluginConcurrentDeploymentClient.create_deployment: Calling GET url {url}")
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            jr = json.loads(response.text)
+            print(jr)
+        except HTTPError as http_err:
+            _logger.info(f'HTTP error occurred: {http_err}')
+            raise
+        except Exception as err:
+            _logger.info(f'Other error occurred: {err}')
+            raise
+        return jr
 
     def get_deployment(self, name, endpoint=None):
         print(f"PluginConcurrentDeploymentClient.get_deployment: name={name}, endpoint={endpoint}")
